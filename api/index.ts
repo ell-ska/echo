dd4b81ce277e7343ca9d4fe79bfb9c39a0fe4ea5
ell-ska <ella@skagerstrom.se>
2025-03-04 13:41:35 +0100

    fix: connect routes

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 
+import { router } from './routes'
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use(router)
 
 app.listen(process.env.PORT, () => {
   console.log(`server ready on port ${process.env.PORT}`)
