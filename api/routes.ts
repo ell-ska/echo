001addfb52d58c88aa0f2443d9f477af428661b5
ell-ska <ella@skagerstrom.se>
2025-03-04 11:09:37 +0100

    feat: routes

---
+++ api/routes.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express'
+
+export const router = Router()
+
+// auth router
+
+const authRouter = Router()
+
+authRouter.post('/register')
+authRouter.post('/log-in')
+authRouter.get('/log-out')
+authRouter.post('/token/refresh')
+
+router.use('/auth', authRouter)
+
+// capsule router
+
+const capsuleRouter = Router()
+
+capsuleRouter.post('/')
+capsuleRouter.get('/public')
+capsuleRouter.get('/user/:id')
+capsuleRouter.get('/user/:id/recieved')
+capsuleRouter.get('/user/:id/sent')
+capsuleRouter.get('/:id')
+capsuleRouter.put('/:id')
+capsuleRouter.delete('/:id')
+
+router.use('/capsules', capsuleRouter)
+
+// user router
+
+const userRouter = Router()
+
+userRouter.get('/:id')
+userRouter.get('/me')
+userRouter.put('/me')
+userRouter.delete('/me')
+
+router.use('/users', userRouter)
