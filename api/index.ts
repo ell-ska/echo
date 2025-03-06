cc327995577c7ee30f6f4c5dd26494c8d944fd37
ell-ska <ella@skagerstrom.se>
2025-03-06 10:37:51 +0100

    feat: add helmet

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 import 'dotenv/config'
 
 import { router } from './routes'
 
 const app = express()
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
 
 app.use(router)
