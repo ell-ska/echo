6419b152debcb2b072edf2cf3669936ab60430b6
Ella <96110484+ell-ska@users.noreply.github.com>
2025-04-15 19:15:59 +0200

    feat(client): register page (#168)

---
+++ packages/client/src/components/register-form.ts
@@ -0,0 +1,100 @@
+import { z } from 'zod'
+
+import { registerSchema } from '@repo/validation/actions'
+import { ComponentWithMutation } from '../core/component'
+import { element } from '../utils/element'
+import { client } from '../lib/client'
+import { router } from '../main'
+import { Button } from '../components/button'
+import { Input } from '../components/input'
+import { Toaster } from '../components/toaster'
+
+const schema = registerSchema
+  .merge(z.object({ confirm: z.string() }))
+  .refine(({ password, confirm }) => password === confirm, {
+    message: 'passwords do not match',
+    path: ['confirm'],
+  })
+
+type RegisterValues = z.infer<typeof schema>
+
+export class RegisterForm extends ComponentWithMutation<RegisterValues> {
+  constructor() {
+    super({})
+  }
+
+  protected schema = () => schema
+
+  protected async mutation(values: RegisterValues) {
+    await client.post('/auth/register', values)
+    router.navigate('/')
+  }
+
+  protected onError(message: string) {
+    Toaster.show(message)
+  }
+
+  render() {
+    const username = new Input({
+      label: 'Username',
+      name: 'username',
+      value: this.values.username || '',
+      error: this.validationErrors?.username?._errors[0],
+    })
+
+    const email = new Input({
+      label: 'Email',
+      name: 'email',
+      type: 'email',
+      value: this.values.email || '',
+      error: this.validationErrors?.email?._errors[0],
+    })
+
+    const password = new Input({
+      label: 'Password',
+      name: 'password',
+      type: 'password',
+      value: this.values.password || '',
+      error: this.validationErrors?.password?._errors[0],
+    })
+
+    const confirm = new Input({
+      label: 'Confirm password',
+      name: 'confirm',
+      type: 'password',
+      value: this.values.confirm || '',
+      error: this.validationErrors?.confirm?._errors[0],
+    })
+
+    const button = new Button({
+      label: 'Create account',
+    })
+
+    const inputs = element('div', {
+      className: 'flex flex-col gap-6',
+      children: [
+        username.element,
+        email.element,
+        password.element,
+        confirm.element,
+      ],
+    })
+
+    const form = element('form', {
+      on: {
+        submit: (event: SubmitEvent) => {
+          event.preventDefault()
+
+          const formData = new FormData(form)
+          const values = Object.fromEntries(formData.entries())
+
+          this.mutate(values)
+        },
+      },
+      className: 'flex flex-col gap-8 w-full',
+      children: [inputs, button.element],
+    })
+
+    return form
+  }
+}
--- packages/validation/src/actions.ts
+++ packages/validation/src/actions.ts
@@ -4,8 +4,8 @@ import { imageSchema, passwordSchema, usernameSchema } from './partials'
 
 export const registerSchema = z.object({
   username: usernameSchema,
-  firstName: z.string(),
-  lastName: z.string(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
   email: z.string().email(),
   password: passwordSchema,
   image: imageSchema.optional(),
