56550fbd9cfba94e498c4beab72289757108fdfc
ell-ska <ella@skagerstrom.se>
2025-04-20 15:19:18 +0200

    feat(client): add tertiary button variant

--- packages/client/src/components/button.ts
+++ packages/client/src/components/button.ts
@@ -6,30 +6,34 @@ import { element } from '../utils/element'
 import { cn } from '../utils/classnames'
 
 const variants = cva(
-  'flex gap-2 items-center justify-center rounded-full bg-zinc-800 text-white border border-transparent outline-primary-bright cursor-pointer whitespace-nowrap transition hover:bg-zinc-700',
+  'flex gap-2 items-center justify-center rounded-full border border-transparent outline-primary-bright cursor-pointer whitespace-nowrap transition',
   {
     variants: {
       variant: {
-        primary: '',
+        primary: ['bg-zinc-800 text-white', 'hover:bg-zinc-700'],
         secondary: [
           'bg-white text-zinc-800 border-zinc-800',
           'hover:bg-zinc-800 hover:text-white',
         ],
-        round: '',
+        tertiary: ['bg-transparent text-zinc-800', 'hover:bg-slate-200'],
       },
       size: {
         sm: 'text-xs py-2 px-4',
         md: 'text-base py-2 px-6',
       },
+      shape: {
+        pill: '',
+        round: '',
+      },
     },
     compoundVariants: [
       {
-        variant: 'round',
+        shape: 'round',
         size: 'sm',
         class: 'size-6 p-0',
       },
       {
-        variant: 'round',
+        shape: 'round',
         size: 'md',
         class: 'size-8 p-0',
       },
@@ -40,7 +44,7 @@ const variants = cva(
 type Props = {
   label?: string
   icon?: IconNode
-  variant?: 'primary' | 'secondary'
+  variant?: 'primary' | 'secondary' | 'tertiary'
   size?: 'sm' | 'md'
   href?: string
   onClick?: (event: MouseEvent) => void
@@ -76,7 +80,9 @@ export class Button extends Component<Props> {
       : null
 
     const children = [icon, span]
-    const className = cn(variants({ variant: icon ? 'round' : variant, size }))
+    const className = cn(
+      variants({ variant, shape: icon ? 'round' : 'pill', size })
+    )
 
     if (href) {
       return element('a', {
