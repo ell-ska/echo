f6e531b2f923835bfd79ed98f25ccb9f9d618440
ell-ska <ella@skagerstrom.se>
2025-04-20 15:05:32 +0200

    refactor(client): make Header more reusable

--- packages/client/src/components/header.ts
+++ packages/client/src/components/header.ts
@@ -1,31 +1,20 @@
-import { Button } from './button'
 import { Component } from '../core/component'
-import { element } from '../utils/element'
+import { type Child, element } from '../utils/element'
 
-export class Header extends Component {
-  constructor() {
-    super({})
+type Props = {
+  children: Child[]
+}
+
+export class Header extends Component<Props> {
+  constructor(props: Props) {
+    super({ props })
   }
 
   render() {
-    const logo = element('a', {
-      innerText: 'echo',
-      href: '/',
-      className: 'text-lg font-black',
-    })
-
-    const button = new Button({
-      label: 'Log in',
-      size: 'sm',
-      href: '/auth/log-in',
-    })
-
-    const header = element('header', {
+    return element('header', {
       className:
         'fixed z-40 w-full flex justify-between items-center max-w-5xl px-4 h-header-sm bg-slate-50/80 backdrop-blur-xs md:h-header-md',
-      children: [logo, button.element],
+      children: this.props.children,
     })
-
-    return header
   }
 }
