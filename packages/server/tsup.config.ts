698f4d9fc9feb462eb0f26792453c7534af3ab1e
ell-ska <ella@skagerstrom.se>
2025-04-23 11:15:40 +0200

    fix: build

--- packages/server/tsup.config.ts
+++ packages/server/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['api/index.ts'],
   outDir: 'dist',
-  format: ['cjs'],
+  format: ['esm'],
   target: 'node18',
   splitting: false,
   clean: true,
