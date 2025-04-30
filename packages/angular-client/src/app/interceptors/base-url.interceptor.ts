6d7a67db9b57a3e0dbf6b114087e8ab65a99bdd3
Ella <96110484+ell-ska@users.noreply.github.com>
2025-04-30 11:27:50 +0200

    feat: set up http client (#255)

---
+++ packages/angular-client/src/app/interceptors/base-url.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
+  if (/^https?:\/\//i.test(req.url)) {
+    return next(req);
+  }
+
+  const leadingAndTrailingSlashRegex = /^\/|\/$/g;
+
+  const apiReq = req.clone({
+    url: `${environment.serverUrl.replace(leadingAndTrailingSlashRegex, '')}/${req.url.replace(leadingAndTrailingSlashRegex, '')}`,
+  });
+  return next(apiReq);
+};
