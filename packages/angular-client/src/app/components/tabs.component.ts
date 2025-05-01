f4688f216d9b1deb0d1554032e28b4db86e6b32a
Ella <96110484+ell-ska@users.noreply.github.com>
2025-05-01 15:33:45 +0200

    feat: create tabs (#258)

---
+++ packages/angular-client/src/app/components/tabs.component.ts
@@ -0,0 +1,37 @@
+import { Component, computed, input } from '@angular/core';
+import { Params, RouterLink, RouterLinkActive } from '@angular/router';
+
+import { cn } from '../../utils/classname';
+
+@Component({
+  selector: 'app-tabs',
+  imports: [RouterLink, RouterLinkActive],
+  template: `
+    <div [class]="containerClasses()">
+      @for (tab of tabs(); track tab.label) {
+        <a
+          [routerLink]="tab.routerLink"
+          [queryParams]="tab.params"
+          [routerLinkActive]="'!bg-slate-800 !text-white hover:!bg-slate-700'"
+          [routerLinkActiveOptions]="{ exact: true }"
+          class="py-2 px-3 text-xs text-zinc-600 bg-white rounded-full transition hover:bg-slate-100"
+          >{{ tab.label }}</a
+        >
+      }
+    </div>
+  `,
+})
+export class TabsComponent {
+  tabs = input.required<
+    {
+      label: string;
+      routerLink: string;
+      params?: Params;
+    }[]
+  >();
+  classes = input<string>();
+
+  containerClasses = computed(() =>
+    cn('flex gap-2 p-1 bg-white rounded-full shadow-float', this.classes()),
+  );
+}
