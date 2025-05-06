3e61dfece31f756ec3e4b7dcf95f70933f29ba18
Ella <96110484+ell-ska@users.noreply.github.com>
2025-05-06 11:58:01 +0200

    feat: toast component (#262)

--- packages/angular-client/src/app/app.component.ts
+++ packages/angular-client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastComponent } from './components/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  imports: [RouterOutlet, ToastComponent],
+  template: ` <router-outlet /> <app-toast /> `,
   styles: [],
 })
 export class AppComponent {}
