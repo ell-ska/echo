ae0e4f6b8c0dc565da685a33ffbcfe2ddf06e92a
ell-ska <ella@skagerstrom.se>
2025-05-09 17:13:53 +0200

    feat: add capsule editor wizard routes

---
+++ packages/angular-client/src/app/pages/capsule-wizard/content.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  imports: [],
+  template: `
+    <p>
+      content works!
+    </p>
+  `,
+  styles: ``
+})
+export class ContentComponent {
+
+}
