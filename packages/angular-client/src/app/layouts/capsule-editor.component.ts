4f983673363decda50e2b8d16fb6bce2feb18ec3
Ella <96110484+ell-ska@users.noreply.github.com>
2025-05-12 18:17:22 +0200

    feat: capsule editor content (#286)

--- packages/angular-client/src/app/layouts/capsule-editor.component.ts
+++ packages/angular-client/src/app/layouts/capsule-editor.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { startWith } from 'rxjs';
+import { ChevronLeft, X } from 'lucide-angular';
+
+import { CapsuleEditorService } from '../services/capsule-editor.service';
+import { HeaderComponent } from '../components/header.component';
+import { ButtonComponent } from '../components/button.component';
+
+const defaultTitle = 'Untitled capsule';
 
 @Component({
   selector: 'app-capsule-editor-layout',
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  imports: [RouterOutlet, HeaderComponent, ButtonComponent],
+  template: `
+    <app-header classes="grid grid-cols-[20%_60%_20%] items-center">
+      @if (capsuleEditor.getStep() === 'content') {
+        <app-button [icon]="x" variant="tertiary" href="/" />
+      } @else {
+        <app-button
+          [icon]="chevronLeft"
+          variant="tertiary"
+          (onClick)="capsuleEditor.back()"
+        />
+      }
+      <h1 class="font-bold text-center truncate">{{ title() }}</h1>
+    </app-header>
+    <main class="main">
+      <router-outlet />
+    </main>
+  `,
 })
-export class CapsuleEditorLayoutComponent {}
+export class CapsuleEditorLayoutComponent {
+  title = signal(defaultTitle);
+  x = X;
+  chevronLeft = ChevronLeft;
+
+  capsuleEditor = inject(CapsuleEditorService);
+
+  ngOnInit() {
+    this.capsuleEditor.redirectToLastStep();
+
+    this.capsuleEditor.form.controls.title.valueChanges
+      .pipe(
+        startWith(this.capsuleEditor.form.controls.title.value || defaultTitle),
+      )
+      .subscribe((value) => {
+        if (!value) {
+          return this.title.set(defaultTitle);
+        }
+        this.title.set(value);
+      });
+  }
+}
--- packages/angular-client/src/app/pages/capsule-wizard/content.component.ts
+++ packages/angular-client/src/app/pages/capsule-wizard/content.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { CapsuleEditorService } from '../../services/capsule-editor.service';
+import { MultipleComponent } from '../../components/upload/multiple.component';
+import { InputComponent } from '../../components/input.component';
+import { ButtonComponent } from '../../components/button.component';
 
 @Component({
   selector: 'app-content',
-  imports: [],
-  template: ` <p>content works!</p> `,
-  styles: ``,
+  imports: [
+    ReactiveFormsModule,
+    MultipleComponent,
+    InputComponent,
+    ButtonComponent,
+  ],
+  template: `
+    <form [formGroup]="form" class="w-full max-w-sm flex flex-col gap-8">
+      <div class="flex flex-col gap-6">
+        <app-input
+          [control]="form.controls.title"
+          name="title"
+          label="Title"
+          [error]="this.capsuleEditor.getValidationError('title')"
+        />
+        <app-input
+          [textarea]="true"
+          [control]="form.controls.content"
+          name="content"
+          label="Content"
+          [error]="this.capsuleEditor.getValidationError('content')"
+        />
+      </div>
+      <app-upload-multiple
+        name="images"
+        label="Images"
+        (files)="capsuleEditor.images.set($event)"
+      />
+    </form>
+
+    <nav class="capsule-editor-navigation">
+      <app-button label="Next" (onClick)="capsuleEditor.next()" />
+    </nav>
+  `,
 })
-export class ContentComponent {}
+export class ContentComponent {
+  capsuleEditor = inject(CapsuleEditorService);
+  form = this.capsuleEditor.form;
+}
