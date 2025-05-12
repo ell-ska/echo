4c748756f5e0d074085a8d8d340b67c315f23b33
ell-ska <ella@skagerstrom.se>
2025-05-12 17:40:00 +0200

    refactor: move header styling

--- packages/angular-client/src/app/components/header.component.ts
+++ packages/angular-client/src/app/components/header.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+import { cn } from '../../utils/classname';
 
 @Component({
   selector: 'app-header',
   template: `
     <header
-      class="fixed z-40 left-1/2 -translate-x-1/2 w-full flex justify-between items-center max-w-5xl px-4 h-header-sm bg-slate-50/80 backdrop-blur-xs md:h-header-md"
+      [class]="
+        cn(
+          'fixed z-40 left-1/2 -translate-x-1/2 w-full max-w-5xl px-4 h-header-sm bg-slate-50/80 backdrop-blur-xs md:h-header-md',
+          classes()
+        )
+      "
     >
       <ng-content />
     </header>
   `,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  classes = input<string>();
+  cn = cn;
+}
