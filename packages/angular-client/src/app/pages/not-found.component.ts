f7abe88d92cec0ece695f4fcda725cae458b7136
ell-ska <ella@skagerstrom.se>
2025-05-14 20:09:42 +0200

    feat: not found page

--- packages/angular-client/src/app/pages/not-found.component.ts
+++ packages/angular-client/src/app/pages/not-found.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
+import { ButtonComponent } from '../components/button.component';
+
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
-  template: ` <p>not-found works!</p> `,
+  imports: [ButtonComponent],
+  template: `
+    <div class="flex flex-col items-center justify-center gap-8 min-h-screen">
+      <div class="flex flex-col items-center gap-1 text-center">
+        <h1 class="text-xl font-bold">Oops, looks like you got lost!</h1>
+        <p>The page you were looking for was not found.</p>
+      </div>
+      <app-button label="Go back home" href="/" />
+    </div>
+  `,
 })
 export class NotFoundPageComponent {}
